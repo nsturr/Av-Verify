600b5c60b1ec3df4505e93474f25e1c2cbcc64d0
Scevine <esbe.v2@gmail.com>
2013-11-02 15:32:35 -0400

    Adds specs for Helps

--- spec/helps_spec.rb
+++ spec/helps_spec.rb
@@ -1,6 +1,23 @@
+require "./spec/spec_helper"
 require "./lib/sections/helps"
 
+help = File.read("./spec/test-helps.are")
+
 describe Helps do
+
+  let(:helps) { Helps.new(help) }
+
+  it "detects invalid text after the delimiter" do
+    helps.contents << "\n\nHey, good times man."
+
+    expect_one_error(helps, Helps.err_msg(:continues_after_delimiter))
+  end
+
+  it "detects a missing delimiter" do
+    helps.contents.slice!(/0\$~.*\z/m)
+
+    expect_one_error(helps, Helps.err_msg(:no_delimiter))
+  end
 
 end
 
