61f91bd7f9aca4a5f03c6b8fe6655054760831d5
Scevine <esbe.v2@gmail.com>
2013-11-02 13:22:28 -0400

    Puts AREADATA error messages in hash

--- lib/sections/area_data.rb
+++ lib/sections/area_data.rb
@@ -1,10 +1,6 @@
 require_relative 'section'
 require_relative '../helpers/avconstants.rb'
 
-# TODO: Assign instnace vars and readers to AreaData so we can fetch
-# the data later on.
-# TODO: dry up the duplication checking
-
 class AreaData < Section
 
   @ERROR_MESSAGES = {
@@ -12,8 +8,15 @@ class AreaData < Section
     continues_after_delimeter: "Section continues after 'S' delimeter",
     no_delimeter: "#AREADATA lacks terminating S",
     duplicate: "Duplicate '%s' line in #AREADATA",
-    invalid_extra_text: "Invalid text after #AREADATA %s line",
+    invalid_extra_text: "Invalid text after #AREADATA %s",
+    invalid_plane_0: "Invalid area plane: 0",
+    invalid_field: "Invalid (non-numeric) %s",
+    plane_out_of_range: "Areadata plane out of bounds #{PLANE_MIN} to #{PLANE_MAX}",
+    zone_out_of_range: "Areadata zone out of bounds 0 to #{ZONE_MAX}",
+    bad_bit: "%s not a power of 2",
+    bad_line: "Bad %s line in #AREADATA",
     kspawn_no_tilde: "Kspawn lacks terminating ~",
+    kspawn_extra_tilde: "Misplaced tildes in Kspawn line",
     kspawn_text_after_tilde: "Invalid text on kspawn line after terminating ~"
   }
 
@@ -59,6 +62,10 @@ class AreaData < Section
         break #Only need to throw this error once
       end
 
+      if @used_lines.include? line[0]
+        err(@current_line, line, AreaData.err_msg(:duplicate) % line[0])
+      end
+
       case line[0]
       when "P"
         parse_plane_line line
@@ -89,57 +96,45 @@ class AreaData < Section
 
   def parse_plane_line line
     # Plane should match: P # #
-    if @used_lines.include? "P"
-      err(@current_line, line, AreaData.err_msg(:duplicate) % "P")
-    else
-      @used_lines << "P"
-      items = line.split(" ", 4)
-      if items.length > 3
-        err(@current_line, line, AreaData.err_msg(:invalid_extra_text) % "plane")
-      end
+    @used_lines << "P"
+    items = line.split(" ", 4)
+    if items.length > 3
+      err(@current_line, line, AreaData.err_msg(:invalid_extra_text) % "plane")
+    end
 
-      if items.length > 1 && items[1] =~ /^-?\d+$/
-        err(@current_line, line, "Invalid area plane: 0") if items[1].to_i == 0
-        err(@current_line, line, "Areadata plane out of bounds #{PLANE_MIN} to #{PLANE_MAX}") unless items[1].to_i.between?(PLANE_MIN, PLANE_MAX)
+    if items.length > 1 && items[1] =~ /^-?\d+$/
+      err(@current_line, line, AreaData.err_msg(:invalid_plane_0)) if items[1].to_i == 0
+      err(@current_line, line, AreaData.err_msg(:plane_out_of_range)) unless items[1].to_i.between?(PLANE_MIN, PLANE_MAX)
+    else
+      err(@current_line, line, AreaData.err_msg(:invalid_field) % "area plane")
+    end
+    # Only bother checking for the zone field if there are enough bits in the line
+    if items.length > 2
+      if items[2] =~ /^-?\d+$/
+        err(@current_line, line, AreaData.err_msg(:zone_out_of_range)) unless items[2].to_i.between?(0, ZONE_MAX)
       else
-        err(@current_line, line, "Invalid (non-numeric) area plane")
-      end
-      # Only bother checking for the zone field if there are enough bits in the line
-      if items.length > 2
-        if items[2] =~ /^-?\d+$/
-          err(@current_line, line, "Areadata zone out of bounds 0 to #{ZONE_MAX}") unless items[2].to_i.between?(0, ZONE_MAX)
-        else
-          err(@current_line, line, "Invalid (non-numeric) area zone")
-        end
+        err(@current_line, line, AreaData.err_msg(:invalid_field) % "area zone")
       end
     end
   end
 
   def parse_flags_line line
     # Areaflags should match: F #|#
-    if @used_lines.include? "F"
-      err(@current_line, line, "Duplicate \"Areaflags\" line in #AREADATA")
+    @used_lines << "F"
+    items = line.split(" ", 3)
+    if items[1] =~ Bits.pattern
+      err(@current_line, line, AreaData.err_msg(:bad_bit) % "Areaflags") if Bits.new(items[1]).error?
     else
-      @used_lines << "F"
-      items = line.split(" ", 3)
-      if items[1] =~ Bits.pattern
-        err(@current_line, line, "Areaflags not a power of 2") if Bits.new(items[1]).error?
-      else
-        err(@current_line, line, "Bad \"Areaflags\" line in #AREADATA")
-      end
-      err(@current_line, line, "Invalid text after #AREADATA area flags") if items.length > 2
+      err(@current_line, line, AreaData.err_msg(:bad_line) % "area flags")
     end
+    err(@current_line, line, AreaData.err_msg(:invalid_extra_text) % "area flags") if items.length > 2
   end
 
   def parse_outlaw_line line
     # Outlaw should match: O # # # # #
-    if @used_lines.include? "O"
-      err(@current_line, line, "Duplicate \"Outlaw\" line in #AREADATA")
-    else
-      @used_lines << "O"
-      unless line =~ /^O(\s+-?\d+){5}$/
-        err(@current_line, line, "Bad \"Outlaw\" line in #AREADATA")
-      end
+    @used_lines << "O"
+    unless line =~ /^O(\s+-?\d+){5}$/
+      err(@current_line, line, AreaData.err_msg(:bad_line) % "outlaw")
     end
   end
 
@@ -147,43 +142,31 @@ class AreaData < Section
     # Kspawn should match: K # # # # text~
     # The text can span multiple lines, which makes this silly tricky,
     # not to mention ugly...
-    if @used_lines.include? "K"
-      err(@current_line, line, "Duplicate \"Kspawn\" line in #AREADATA")
-    else
-      @used_lines << "K"
-      unless line =~ /^K\s+\d+\s+\d+\s+-?\d+\s+-?\d+/
-        err(@current_line, line, "Bad \"Kspawn\" line in #AREADATA")
-      end
+    @used_lines << "K"
+    unless line =~ /^K\s+\d+\s+\d+\s+-?\d+\s+-?\d+/
+      err(@current_line, line, AreaData.err_msg(:bad_line) % "kspawn")
+    end
 
-      if line.include?("~")
-        err(@current_line, line, "Misplaced tildes in Kspawn line") unless line =~ /^K[^~]*~$/
-      else
-        @kspawn_multiline = true
-      end
+    if line.include?("~")
+      err(@current_line, line, AreaData.err_msg(:kspawn_extra_tilde)) unless line =~ /^K[^~]*~$/
+    else
+      @kspawn_multiline = true
     end
   end
 
   def parse_modifier_line line
     # Modifiers should match: M # # # # # # 0 0
-    if @used_lines.include? "M"
-      err(@current_line, line, "Duplicate \"Area modifier\" line in #AREADATA")
-    else
-      @used_lines << "M"
-      unless line =~ /^M(\s+(-|\+)?\d+){8}$/
-        err(@current_line, line, "Bad \"Area modifier\" line in #AREADATA")
-      end
+    @used_lines << "M"
+    unless line =~ /^M(\s+(-|\+)?\d+){8}$/
+      err(@current_line, line, AreaData.err_msg(:bad_line) % "area modifier")
     end
   end
 
   def parse_group_exp_line line
     # Group exp should match: G # # # # # # # 0
-    if @used_lines.include? "G"
-      err(@current_line, line, "Duplicate \"Group exp\" line in #AREADATA")
-    else
-      @used_lines << "G"
-      unless line =~ /^G(\s+\d+){8}$/
-        err(@current_line, line, "Bad \"Group exp\" line in #AREADATA")
-      end
+    @used_lines << "G"
+    unless line =~ /^G(\s+\d+){8}$/
+      err(@current_line, line, AreaData.err_msg(:bad_line) % "group exp")
     end
   end
 
