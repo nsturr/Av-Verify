16d0bf27a5c416b0fabd2d2f6107e0a0ce897a38
Scevine <esbe.v2@gmail.com>
2013-11-03 13:48:05 -0500

    Changes tabs to spaces (finally)

--- lib/varea.rb
+++ lib/varea.rb
@@ -36,190 +36,190 @@ require './helpers/parsable'
 require './helpers/area_attributes'
 
 %w{
-	area_header area_data helps mobiles
-	objects rooms resets shops specials
+  area_header area_data helps mobiles
+  objects rooms resets shops specials
 }.each { |section| require "./sections/#{section}" }
 
 class Area
-	include Parsable
-	include AreaAttributes
+  include Parsable
+  include AreaAttributes
 
-	def initialize(filename, flags=[])
-		# How 'bout a little FILE, Scarecrow! >:D
-		unless File.exist?(filename)
-			puts "#{filename} not found, skipping."
-			return nil
-		end
+  def initialize(filename, flags=[])
+    # How 'bout a little FILE, Scarecrow! >:D
+    unless File.exist?(filename)
+      puts "#{filename} not found, skipping."
+      return nil
+    end
 
-		data = File.read(filename)
-		total_lines = data.count("\n") + 1
-		data.rstrip!
+    data = File.read(filename)
+    total_lines = data.count("\n") + 1
+    data.rstrip!
 
-		@flags = flags.map {|item| item.downcase.to_sym}
+    @flags = flags.map {|item| item.downcase.to_sym}
 
-		@errors = []
+    @errors = []
 
-		if data.end_with?("\#$")
-			# If the correct ending char is found, strip it completely so none of the
-			# section-parsing methods have to worry about it
-			data.slice!(-2..-1)
-		else
-			err(total_lines, nil, "Area file does not end with \#$")
-		end
+    if data.end_with?("\#$")
+      # If the correct ending char is found, strip it completely so none of the
+      # section-parsing methods have to worry about it
+      data.slice!(-2..-1)
+    else
+      err(total_lines, nil, "Area file does not end with \#$")
+    end
 
-		@main_sections = extract_main_sections(data)
-	end
+    @main_sections = extract_main_sections(data)
+  end
 
-	def verify_all
-		@main_sections.each_value do |section|
-			next if section.parsed?
-			puts "Found ##{section.name} on line #{section.line_number}" if @flags.include?(:debug)
-			section.parse
-			@errors += section.errors
-		end
-	end
+  def verify_all
+    @main_sections.each_value do |section|
+      next if section.parsed?
+      puts "Found ##{section.name} on line #{section.line_number}" if @flags.include?(:debug)
+      section.parse
+      @errors += section.errors
+    end
+  end
 
-	private
+  private
 
-	def get_section(id)
-		@main_sections.find { |section| section.id == id.downcase }
-	end
+  def get_section(id)
+    @main_sections.find { |section| section.id == id.downcase }
+  end
 
-	def extract_main_sections(data)
-		lines_so_far = 1
+  def extract_main_sections(data)
+    lines_so_far = 1
 
-		separated = data.split(/^(?=#[a-zA-Z\$]+)/)
-		sections = {}
+    separated = data.split(/^(?=#[a-zA-Z\$]+)/)
+    sections = {}
 
-		separated.each do |content|
-			# Keep track of how many lines per section, so that the next section
-			# has an accurate line number
-			line_start_section = lines_so_far
-			lines_in_section = content.count("\n")
-			lines_so_far += lines_in_section
+    separated.each do |content|
+      # Keep track of how many lines per section, so that the next section
+      # has an accurate line number
+      line_start_section = lines_so_far
+      lines_in_section = content.count("\n")
+      lines_so_far += lines_in_section
 
-			content.rstrip!
-			# The only "section" that should ever be empty is any leading linebreaks
-			# at the very beginning of an area file, if any
-			next if content.empty?
+      content.rstrip!
+      # The only "section" that should ever be empty is any leading linebreaks
+      # at the very beginning of an area file, if any
+      next if content.empty?
 
-			new_section = make_section(content, line_start_section)
+      new_section = make_section(content, line_start_section)
 
-			if sections.any? { |s| s.class == new_section.class }
-				warn(new_section.line_number, nil, "Another #{new_section.class} section? This bodes ill. Skipping")
-				next
-			end
-			sections[new_section.id] = new_section
-		end
+      if sections.any? { |s| s.class == new_section.class }
+        warn(new_section.line_number, nil, "Another #{new_section.class} section? This bodes ill. Skipping")
+        next
+      end
+      sections[new_section.id] = new_section
+    end
 
-		sections
-	end
+    sections
+  end
 
-	def make_section(content, line_num)
-		# Identify area name
-		first_line = content.match(/^#.*?$/).to_s.rstrip
-		# AREA section has its contents on the same line as the section name
-		if first_line.downcase.start_with?("#area ", "#area\t")
-			name = "AREA"
-		else
-			# Other sections technically can have contents on same line as section,
-			# name, but I'm enforcing good syntax anyway.
-			first_line = content.slice(/\A.*(?:\n|\Z)/).rstrip
-			name = first_line.match(/[a-zA-Z\$]+/).to_s
+  def make_section(content, line_num)
+    # Identify area name
+    first_line = content.match(/^#.*?$/).to_s.rstrip
+    # AREA section has its contents on the same line as the section name
+    if first_line.downcase.start_with?("#area ", "#area\t")
+      name = "AREA"
+    else
+      # Other sections technically can have contents on same line as section,
+      # name, but I'm enforcing good syntax anyway.
+      first_line = content.slice(/\A.*(?:\n|\Z)/).rstrip
+      name = first_line.match(/[a-zA-Z\$]+/).to_s
 
-			if first_line.include?(" ")
-				err(line_start_section, first_line, "Invalid text on same line as section name")
-			end
-		end
+      if first_line.include?(" ")
+        err(line_start_section, first_line, "Invalid text on same line as section name")
+      end
+    end
 
-		unless SECTIONS.include? name.downcase
-			err(line_num, nil, "Invalid section name ##{name}") and return
-		end
+    unless SECTIONS.include? name.downcase
+      err(line_num, nil, "Invalid section name ##{name}") and return
+    end
 
-		case name.downcase
-		when "area"
-			AreaHeader.new(content, line_num)
-		when "areadata"
-			AreaData.new(content, line_num)
-		when "helps"
-			Helps.new(content, line_num)
-		when "mobiles"
-			Mobiles.new(content, line_num)
-		when "objects"
-			Objects.new(content, line_num)
-		when "rooms"
-			Rooms.new(content, line_num)
-		when "resets"
-			Resets.new(content, line_num)
-		when "shops"
-			Shops.new(content, line_num)
-		when "specials"
-			Specials.new(content, line_num)
-		end
-	end
+    case name.downcase
+    when "area"
+      AreaHeader.new(content, line_num)
+    when "areadata"
+      AreaData.new(content, line_num)
+    when "helps"
+      Helps.new(content, line_num)
+    when "mobiles"
+      Mobiles.new(content, line_num)
+    when "objects"
+      Objects.new(content, line_num)
+    when "rooms"
+      Rooms.new(content, line_num)
+    when "resets"
+      Resets.new(content, line_num)
+    when "shops"
+      Shops.new(content, line_num)
+    when "specials"
+      Specials.new(content, line_num)
+    end
+  end
 
 end
 
 if __FILE__ == $PROGRAM_NAME
 
-	if ARGV[0]
-		if File.exist?(ARGV[0])
-			puts "Parsing #{ARGV[0]}..."
-			new_area = Area.new(ARGV[0], ARGV[1..-1])
-			new_area.verify_all
-			new_area.error_report
-		else
-			puts "#{ARGV[0]} not found, skipping."
-		end
-	else
-		puts "Usage: varea filename.are [nocolor|cosmetic|nowarning]"
-	end
+  if ARGV[0]
+    if File.exist?(ARGV[0])
+      puts "Parsing #{ARGV[0]}..."
+      new_area = Area.new(ARGV[0], ARGV[1..-1])
+      new_area.verify_all
+      new_area.error_report
+    else
+      puts "#{ARGV[0]} not found, skipping."
+    end
+  else
+    puts "Usage: varea filename.are [nocolor|cosmetic|nowarning]"
+  end
 
 end
 
 =begin Commenting out the needlessly complex batch verifying function.
-				If you're going to uncomment this bit, be sure to comment out the
-				"if" block above
+        If you're going to uncomment this bit, be sure to comment out the
+        "if" block above
 if ARGV[0]
-	files_enumerated = []
-	path = "."
-	
-	# If file to parse ends with .lst, then treat it as area.lst containing
-	# one areafile name per line, ending with End or $
-	if ARGV[0].match(/^[\w\.\\\/_]+\.lst$/i)
-		list_file = File.open(ARGV[0])
-		# Getting the full path of the list file, then putting just the directory
-		# structure into path (i.e. full path minus file name)
-		path = File.dirname(list_file.path)
-		list_file.each_line do |line|
-			# End filename parsing if the current line contains "end" or starts with "$"
-			break if line.rstrip.match(/^(?:\$|end$)/i)
-			# Add each line to the list of files IF the line is just one word.
-			files_enumerated << path + File::SEPARATOR + line.rstrip.chomp if line.rstrip.match(/^[\w\.]+$/)
-		end
-		list_file.close
-	else
-		# Otherwise, this is just a single area file.
-		files_enumerated << ARGV[0]
-	end
+  files_enumerated = []
+  path = "."
+  
+  # If file to parse ends with .lst, then treat it as area.lst containing
+  # one areafile name per line, ending with End or $
+  if ARGV[0].match(/^[\w\.\\\/_]+\.lst$/i)
+    list_file = File.open(ARGV[0])
+    # Getting the full path of the list file, then putting just the directory
+    # structure into path (i.e. full path minus file name)
+    path = File.dirname(list_file.path)
+    list_file.each_line do |line|
+      # End filename parsing if the current line contains "end" or starts with "$"
+      break if line.rstrip.match(/^(?:\$|end$)/i)
+      # Add each line to the list of files IF the line is just one word.
+      files_enumerated << path + File::SEPARATOR + line.rstrip.chomp if line.rstrip.match(/^[\w\.]+$/)
+    end
+    list_file.close
+  else
+    # Otherwise, this is just a single area file.
+    files_enumerated << ARGV[0]
+  end
 
-	areas = {}
-	files_enumerated.each do |file|
-		unless File.exist?(file)
-			puts "#{file} not found, skipping."
-			next
-		end
-		#puts file
-		areas[file] = Area.new(file, ARGV[1..-1])
-	end
+  areas = {}
+  files_enumerated.each do |file|
+    unless File.exist?(file)
+      puts "#{file} not found, skipping."
+      next
+    end
+    #puts file
+    areas[file] = Area.new(file, ARGV[1..-1])
+  end
 
-	areas.each do |file, area|
-		next if area.nil?
-		puts "Results for #{file}:"
-		area.verify_all
-		area.error_report
-	end
+  areas.each do |file, area|
+    next if area.nil?
+    puts "Results for #{file}:"
+    area.verify_all
+    area.error_report
+  end
 else
-	puts "Bzzzz".Y+"ZZZZZ".BY+"zz".Y+"ZZ".BY+"zzzzzzzz".Y+"ZZZZZ".BY+"zzzzz".Y
+  puts "Bzzzz".Y+"ZZZZZ".BY+"zz".Y+"ZZ".BY+"zzzzzzzz".Y+"ZZZZZ".BY+"zzzzz".Y
 end
 =end
