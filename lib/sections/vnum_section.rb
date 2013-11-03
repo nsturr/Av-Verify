c192b732a784cc68230f7c526502e456a1a28593
Scevine <esbe.v2@gmail.com>
2013-11-03 15:58:24 -0500

    Fixes vnum section bug on whitespace

--- lib/sections/vnum_section.rb
+++ lib/sections/vnum_section.rb
@@ -2,6 +2,14 @@ require_relative "section"
 
 class VnumSection < Section
 
+  @ERROR_MESSAGES = {
+    invalid_vnum: "Invalid %s VNUM",
+    duplicate: "Duplicate %s #%i, first appears on line %i",
+    no_delimiter: "#%s section lacks terminating #0",
+    continues_after_delimiter: "#%s section continues after terminating #0",
+    empty: "%s section is empty!"
+  }
+
   def initialize(contents, line_number)
     super(contents, line_number)
     @raw_entries = [] # Unparsed, has no VNUM yet, etc.
@@ -17,37 +25,51 @@ class VnumSection < Section
   def split_entries
 
     @delimiter = slice_delimiter
-
     @current_line += 1
-    section_end = false
 
-    entries = @contents.split(/^(?=#\d\S*)/)
+    entries = @contents.rstrip.split(/^(?=#\d\S*)/)
 
     entries.each do |entry|
+      entry_line_number = @current_line
+      @current_line += entry.count("\n")
+
+      # Only happens if whitespace between header and 1st vnum
+      next if entry.rstrip.empty?
       unless entry =~ /\A#\d+\b/ # bad VNUM
-        err(@current_line, entry[/\A.*$/], "Invalid #{self.class.name} VNUM")
+        err(entry_line_number, entry[/\A.*$/], VnumSection.err_msg(:invalid_vnum) % self.id.upcase)
         next
       end
-      @raw_entries << self.class.child_class.new(entry, @current_line)
-      @current_line += entry.count("\n")
+      @raw_entries << self.class.child_class.new(entry, entry_line_number)
     end
   end
 
   def parse
     super # set @parsed to true
 
+    if @raw_entries.empty?
+      err(self.line_number, nil, VnumSection.err_msg(:empty) % self.class.name.capitalize)
+      @parsed = true
+    end
+
     @raw_entries.each do |entry|
       entry.parse
-      @entries[entry.vnum] = entry
+      unless @entries.key? entry.vnum
+        @entries[entry.vnum] = entry
+      else
+        err(
+          entry.line_number, nil, VnumSection.err_msg(:duplicate) %
+          [entry.class.name.downcase, entry.vnum, @entries[entry.vnum].line_number]
+        )
+      end
       @errors += entry.errors
     end
 
     if @delimiter.nil?
-      err(@current_line, nil, "##{self.class.name} section lacks terminating #0")
+      err(@current_line, nil, VnumSection.err_msg(:no_delimiter) % self.id.upcase)
     else
       unless @delimiter.rstrip =~ /#{self.class.delimiter(:start)}\z/
         line_num, bad_line = invalid_text_after_delimiter(@current_line, @delimiter)
-        err(line_num, bad_line, "##{self.class.name} section continues after terminating #0")
+        err(line_num, bad_line, VnumSection.err_msg(:continues_after_delimiter) % self.id.updase)
       end
     end
     @entries
