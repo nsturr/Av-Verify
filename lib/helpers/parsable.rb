2567c666c77cf94f05b3cd8bdaae503b5e584efa
Scevine <esbe.v2@gmail.com>
2013-11-28 21:53:29 -0500

    Parsable shifts to @errors instead of self.errors

--- lib/helpers/parsable.rb
+++ lib/helpers/parsable.rb
@@ -26,25 +26,25 @@ module Parsable
 
   def err(line, context, description)
     error = Error.new(line, :error, context, description)
-    self.errors << error
+    @errors << error
   end
 
   # Returns a new Error struct, but only for non-critical errors
   def warn(line, context, description)
     error = Error.new(line, :warning, context, description)
-    self.errors << error
+    @errors << error
   end
 
   # Nothing creates these yet, so ignore
   def nb(line, context, description)
     error = Error.new(line, :nb, context, description)
-    self.errors << error
+    @errors << error
   end
 
   # The least important errors, primarily cosmetic things
   def ugly(line, context, description)
     error = Error.new(line, :ugly, context, description)
-    self.errors << error
+    @errors << error
   end
 
   # def errors
