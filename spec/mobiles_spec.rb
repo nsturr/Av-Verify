24e1fb3d20d5c783b22c74b6fe4bb6827be6db10
Scevine <esbe.v2@gmail.com>
2013-11-29 23:01:33 -0500

    Adds pending mobile specs

--- spec/mobiles_spec.rb
+++ spec/mobiles_spec.rb
@@ -31,7 +31,101 @@ describe Mobile do
   #   let(:item) { mobile }
   # end
 
-  context "parsing a mobile" do
+  context "parsing text fields" do
+
+    it "detects a tab character"
+
+    it "detects a short desc that spans more than one line"
+
+    it "detects a long desc that spans more than one line"
+
+    it "detects a missing tilde in the description field"
+
+  end
+
+  context "parsing act/aff/align line" do
+
+    it "detects a missing 'S'"
+
+    it "detects a missing ACT_NPC flag"
+
+    it "detects a bad act bit"
+
+    it "detects a non-numeric act field"
+
+    it "detects a bad affect bit"
+
+    it "detects a non-numeric affect field"
+
+    it "detects an invalid align field"
+
+    it "detects an out-of-range alignment"
+
+    it "detects invalid line syntax"
+
+  end
+
+  context "parsing those boring middle lines" do
+
+    it "detects an invalid mob level"
+
+    it "detects wrong level line syntax"
+
+    it "detects wrong '0d0+0' line syntax"
+
+    it "detects invalid sex field"
+
+    it "detects invalid sex line syntax"
+
+  end
+
+  context "parsing misc fields" do
+
+    it "detects an out-of-range race"
+
+    it "detects an invalid race"
+
+    it "detects invalid text after the race field"
+
+    it "detects a duplicated race field"
+
+    it "detects an out-of-range class"
+
+    it "detects an invalid class"
+
+    it "detects invalid text after the class field"
+
+    it "detects a duplicated class field"
+
+    it "detects an out-of-range team"
+
+    it "detects an invalid team"
+
+    it "detects invalid text after the team field"
+
+    it "detects a duplicated team field"
+
+    it "detects an invalid misc field"
+
+  end
+
+  context "parsing a kspawn" do
+
+    it "detects a duplicated kspawn field"
+
+    it "detects invalid kspawn syntax"
+
+    it "detects a bad kspawn condition"
+
+    it "detects a bad kspawn type bit"
+
+    it "detects a bad kspawn vnum"
+
+    it "detects a bad kspawn location"
+
+    it "detects a visible tab"
+
+    it "detects a kspawn with a missing tilde"
 
   end
 
